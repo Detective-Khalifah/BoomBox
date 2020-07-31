0f3594fbd73433e638f9348010c7c6d776112e99
Detective_Khalifah <detective.khalifah.dtcfscd@gmail.com>
2020-07-31 20:16:14 +0100

    Update dependencies Add test file to play by android.media.MediaPlayer

--- app/src/main/java/hub/programs/boombox/MainActivity.java
+++ app/src/main/java/hub/programs/boombox/MainActivity.java
@@ -2,13 +2,19 @@ package hub.programs.boombox;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.media.MediaPlayer;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
+
+    MediaPlayer theMediaPlayer;
 
     @Override
     protected void onCreate (Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        theMediaPlayer = MediaPlayer.create(MainActivity.this.getBaseContext(), R.raw.traveling);
+        theMediaPlayer.start();
     }
 }
